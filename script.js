8a1844057ebcb0096618fd41d28fc1a80a06f1fb
Ankit singh <167414480+AuthAnkit@users.noreply.github.com>
2025-12-17 22:08:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ let ptag = document.getElementById("status");
 let but = document.getElementById("enterBtn");
 
 but.addEventListener("click",function(){
-	ptag.innerHTML="<h1>Entered MetaVerse</h1>";
+	ptag.innerHTML="<h1>Entered Metaverse</h1>";
 });
