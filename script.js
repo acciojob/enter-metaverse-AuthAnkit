29e42f15096cf2ccb77a3becaf0fbe0ce05827fe
Ankit singh <167414480+AuthAnkit@users.noreply.github.com>
2025-12-17 21:52:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+let ptag = document.getElementById("text");
+let but = document.getElementById("btn");
+
+but.addEventListener("click",function(){
+	ptag.innerHTML="<h1>Entered MetaVerse</h1>";
+});
